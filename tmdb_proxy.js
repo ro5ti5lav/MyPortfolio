5948b5b0af22c314b23d5a4d79ff9c64d71f2920
Mikhail Kushnir <ro5ti5lav@yandex.ru>
2024-12-28 11:44:02 +0400

    tmdb ver.2

--- tmdb_proxy.js
+++ tmdb_proxy.js
@@ -1,25 +1,39 @@
-(function () {
-    'use strict';
+let tmdb_proxy = {
+  name: 'TMDB Proxy',
+  version: '1.0.3',
+  description: 'Проксирование постеров и API сайта TMDB',
 
-    var tmdb_proxy = {
-      name: 'TMDB Proxy',
-      version: '1.0.1',
-      description: 'Проксирование постеров и API сайта TMDB',
-      path_image: 'http://imagetmdb.com/',
-      path_api: 'http://apitmdb.cub.watch/3/'
-    };
-    Lampa.TMDB.image = function (url) {
-      var base = Lampa.Utils.protocol() + 'image.tmdb.org/' + url;
-      return Lampa.Storage.field('proxy_tmdb') ? tmdb_proxy.path_image + url : base;
-    };
-    Lampa.TMDB.api = function (url) {
-      var base = Lampa.Utils.protocol() + 'api.themoviedb.org/3/' + url;
-      return Lampa.Storage.field('proxy_tmdb') ? tmdb_proxy.path_api + url : base;
-    };
-    Lampa.Settings.listener.follow('open', function (e) {
-      if (e.name == 'tmdb') {
-        e.body.find('[data-parent="proxy"]').remove();
-      }
-    });
+  path_image: Lampa.Account.hasPremium() ? 'imagetmdb.cub.red/' : 'imagetmdb.com/',
+  path_api: 'apitmdb.' + (Lampa.Manifest && Lampa.Manifest.cub_domain ? Lampa.Manifest.cub_domain : 'cub.red') + '/3/'
+}
 
-})();
+function filter(u) {
+  let s = u.slice(0, 8)
+  let e = u.slice(8).replace(/\/+/g, '/')
+
+  return s + e
+}
+
+function email() {
+  return Lampa.Storage.get('account', '{}').email || ''
+}
+
+Lampa.TMDB.image = function (url) {
+  let base = Lampa.Utils.protocol() + 'image.tmdb.org/' + url
+
+  return Lampa.Utils.addUrlComponent(filter(Lampa.Storage.field('proxy_tmdb') ? Lampa.Utils.protocol() + tmdb_proxy.path_image + url : base), 'email=' + encodeURIComponent(email()))
+}
+
+Lampa.TMDB.api = function (url) {
+  let base = Lampa.Utils.protocol() + 'api.themoviedb.org/3/' + url
+
+  return Lampa.Utils.addUrlComponent(filter(Lampa.Storage.field('proxy_tmdb') ? Lampa.Utils.protocol() + tmdb_proxy.path_api + url : base), 'email=' + encodeURIComponent(email()))
+}
+
+Lampa.Settings.listener.follow('open', function (e) {
+  if (e.name == 'tmdb') {
+    e.body.find('[data-parent="proxy"]').remove()
+  }
+})
+
+console.log('TMDB-Proxy', 'started, enabled:', Lampa.Storage.field('proxy_tmdb'))
